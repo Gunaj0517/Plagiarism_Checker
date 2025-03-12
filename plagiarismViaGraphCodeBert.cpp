7343760fcaa4cfded972cff4c22ec9488e8b7216
Gunaj0517 <gunaj.chugh@gmail.com>
2025-03-12 16:10:24 +0530

    Saving local changes before pull

--- plagiarismViaGraphCodeBert.cpp
+++ plagiarismViaGraphCodeBert.cpp
@@ -2,12 +2,20 @@
 #include <cstdlib>
 
 void checkPlagiarism(std::string code1, std::string code2) {
-    // Escaping " and << properly
+    // Escape `<<` for Windows CMD
+    size_t pos;
+    while ((pos = code1.find("<<")) != std::string::npos) {
+        code1.replace(pos, 2, "^<^<");
+    }
+    while ((pos = code2.find("<<")) != std::string::npos) {
+        code2.replace(pos, 2, "^<^<");
+    }
+
     std::string command = "curl -X POST http://127.0.0.1:5000/compare "
                           "-H \"Content-Type: application/json\" "
                           "-d \"{\\\"code1\\\": \\\"" + code1 + "\\\", \\\"code2\\\": \\\"" + code2 + "\\\"}\"";
 
-    std::cout << "Executing command: " << command << std::endl;  // Debugging output
+    std::cout << "Executing command: " << command << std::endl;
     int result = system(command.c_str());
 
     if (result != 0) {
