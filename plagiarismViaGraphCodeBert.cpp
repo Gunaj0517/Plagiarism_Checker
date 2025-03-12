02de130b874570a46db3b9acb232ae76638811ab
Gunaj0517 <gunaj.chugh@gmail.com>
2025-03-12 15:51:46 +0530

    PlagiarismCheckerViaC++

---
+++ plagiarismViaGraphCodeBert.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+
+void checkPlagiarism(std::string code1, std::string code2) {
+    // Escaping " and << properly
+    std::string command = "curl -X POST http://127.0.0.1:5000/compare "
+                          "-H \"Content-Type: application/json\" "
+                          "-d \"{\\\"code1\\\": \\\"" + code1 + "\\\", \\\"code2\\\": \\\"" + code2 + "\\\"}\"";
+
+    std::cout << "Executing command: " << command << std::endl;  // Debugging output
+    int result = system(command.c_str());
+
+    if (result != 0) {
+        std::cerr << "Error executing curl command!" << std::endl;
+    }
+}
+
+int main() {
+    std::string code1 = "int main() { int a = 5; std::cout << a; return 0; }";
+    std::string code2 = "int main() { int x = 5; std::cout << x; return 0; }";
+
+    checkPlagiarism(code1, code2);
+    return 0;
+}
